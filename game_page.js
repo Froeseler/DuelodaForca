630d60077344cb8e619de81a73fabd34e2825f63
fred <froeseler@hotmail.com>
2024-10-02 21:28:48 -0300

    Alteração no game_page.js

--- game_page.js
+++ game_page.js
@@ -30,3 +30,39 @@ function send(){
     document.getElementById("output").innerHTML= row;
     document.getElementById("word").value = "";
 }
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+
+    if (answer == word){
+        if(answer_turn == "player1"){
+            player1_score += 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }else{
+            player2_score += 1;
+            document.getElementById("player1_score").innerHTML = player2_score;
+        }
+    }
+
+    if(question_turn == "player1"){
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Turno de perguntas: " + player2_name;
+    }else{
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Turno de perguntas: " + player1_name;
+    }
+
+    if(answer_turn == "player1"){
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Turno de resposta: " + player2_name;
+    }else{
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Turno de resposta: " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
